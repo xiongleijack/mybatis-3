1eca9c155aa2d248c449e9d4fe203bc98f69d514
51525 <lei.xiong@innodealing.com>
2022-08-10 07:26:24 +0800

    工厂处理

--- src/main/java/org/apache/ibatis/binding/MapperRegistry.java
+++ src/main/java/org/apache/ibatis/binding/MapperRegistry.java
@@ -42,6 +42,7 @@ public class MapperRegistry {
 
   @SuppressWarnings("unchecked")
   public <T> T getMapper(Class<T> type, SqlSession sqlSession) {
+    // 获取Mapper ProxyFactory 工厂
     final MapperProxyFactory<T> mapperProxyFactory = (MapperProxyFactory<T>) knownMappers.get(type);
     if (mapperProxyFactory == null) {
       throw new BindingException("Type " + type + " is not known to the MapperRegistry.");
